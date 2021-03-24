12a0f0e75ab9ab4955d47aa164b36127057619aa
XQ <xuqiang01@bilibili.com>
2021-03-24 12:54:44 +0800

    init

---
+++ QDarwin/Shared/QDarwinApp.swift
@@ -0,0 +1,17 @@
+//
+//  QDarwinApp.swift
+//  Shared
+//
+//  Created by XQ on 2021/3/24.
+//
+
+import SwiftUI
+
+@main
+struct QDarwinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
